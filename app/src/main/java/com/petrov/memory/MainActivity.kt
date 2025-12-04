fabc134b8be26fc2fe14ac1ace893b97705034b7
beloecoleno <SM.Petrov@urfu.me>
2025-12-04 20:33:28 +0500

    Update MainActivity to launch game

--- app/src/main/java/com/petrov/memory/MainActivity.kt
+++ app/src/main/java/com/petrov/memory/MainActivity.kt
@@ -1,20 +1,19 @@
 package com.petrov.memory
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.petrov.memory.ui.game.GameActivity
 
+/**
+ * Главная Activity - временно запускает игру сразу
+ * TODO: В будущем здесь будет меню (из ТЗ раздел 4.1.1.2)
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        // Пока сразу запускаем игру для тестирования
+        startActivity(Intent(this, GameActivity::class.java))
+        finish()
     }
 }
